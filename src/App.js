4d04cf0f3e18cd2ae5cff4d2500a5f1847c6d829
avutibhavana <avutibhavana06@gmail.com>
2022-08-26 12:58:05 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect,useState} from 'react'
 
-function App() {
+const App = () => {
+  const [posts,setPosts]=useState([])
+  const [checks,setChecks]=useState(false)
+  const [count,setCount]=useState(0)
+
+  let json;
+  useEffect(()=>{
+    const loadAsync=async()=>{
+      try{
+      const response=await fetch("https://jsonplaceholder.typicode.com/users")
+      json=await response.json()
+      console.log(json)
+      setPosts(json)
+      }catch(e){
+        console.log(e)
+      }
+    }
+    loadAsync()
+  },[])
+
+const targetElement=(e)=>{
+
+}
+const check=(e)=>{
+  if(e.target.element===posts.company.name){
+    return true
+  }
+  else{
+    return false
+  }
+  }
+console.log(posts)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+    <form>
+      <label>input:</label>
+      <input type="text" onChange={targetElement}></input>
+      <button onClick={check}>Search</button>
+    checks?"<div><h4>{posts.company.name}</h4></div>
+    <div><h4>{posts.company.email}</h4></div>":"<div><h4>Not found</h4></div>"
+
+    </form>
+
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
